3033f5e32a49a3a27a67b952c2f5ab9c6a949c8c
aksagr1 <142683036+aksagr1@users.noreply.github.com>
2026-01-25 19:46:41 +0530

    Remove base path and build configuration from Vite
    Removed base path configuration and build output directory.

--- EXP-1/EXP1.1/vite.config.js
+++ EXP-1/EXP1.1/vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/EXP1.1/",
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  }
 })
