b001f3c4fcad5dfc48a3973701d04d861c7fa6fd
aksagr1 <142683036+aksagr1@users.noreply.github.com>
2026-01-25 19:35:35 +0530

    Add build output directory to Vite config
    Added build configuration to specify output directory.

--- EXP-1/EXP1.3/vite.config.js
+++ EXP-1/EXP1.3/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: "/EXP1.3/",
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    outDir: 'dist'
+  }
 })
